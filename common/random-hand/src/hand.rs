33d6cb5547d0195e200c679228cf273e29db5214
Apricot-S <mahjong_apricot@outlook.jp>
2024-09-14 09:36:19 +0900

    refactor: Change from Vec to array

--- common/random-hand/src/hand.rs
+++ common/random-hand/src/hand.rs
@@ -2,7 +2,7 @@ use rand::seq::SliceRandom;
 use rand::Rng;
 
 pub fn generate_random_pure_hand(rng: &mut impl Rng) -> [u8; 34] {
-    let mut wall: Vec<u8> = (0..34).flat_map(|x| vec![x; 4]).collect();
+    let mut wall: Vec<u8> = (0..34).flat_map(|x| [x; 4]).collect();
     wall.shuffle(rng);
 
     const CHOICES: [usize; 10] = [1, 2, 4, 5, 7, 8, 10, 11, 13, 14];
@@ -12,5 +12,6 @@ pub fn generate_random_pure_hand(rng: &mut impl Rng) -> [u8; 34] {
     wall.iter()
         .take(hand_length)
         .for_each(|&t| hand[t as usize] += 1);
+
     hand
 }
