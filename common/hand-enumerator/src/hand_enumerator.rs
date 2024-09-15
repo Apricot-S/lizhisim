d0ab906920e185dec26806a4f6b04f4332427886
Apricot-S <mahjong_apricot@outlook.jp>
2024-09-15 22:58:26 +0900

    refactor: Avoid using Rc and Cell

--- common/hand-enumerator/src/hand_enumerator.rs
+++ common/hand-enumerator/src/hand_enumerator.rs
@@ -1,22 +1,24 @@
-use std::{cell::Cell, rc::Rc};
+const MAX_NUM_SAME_TILE: u8 = 4;
+const NUM_TILE_INDEX: usize = 3 * 9 + 4 + 3;
+const MAX_NUM_HAND: usize = 14;
 
 pub struct HandEnumerator {
-    tiles: Vec<usize>,
+    tiles: [u8; NUM_TILE_INDEX],
     current_hand: Vec<u8>,
-    stack: Vec<(usize, Rc<Cell<usize>>)>,
+    stack: Vec<usize>,
     length: usize,
 }
 
 impl HandEnumerator {
     pub fn new(length: usize) -> Result<Self, String> {
-        if length < 1 || length > 14 {
+        if length < 1 || length > MAX_NUM_HAND {
             return Err(format!("Hand length must be between 1 and 14.: {}", length));
         }
 
         Ok(Self {
-            tiles: vec![4; 34],
+            tiles: [MAX_NUM_SAME_TILE; NUM_TILE_INDEX],
             current_hand: Vec::new(),
-            stack: vec![(0, Rc::new(Cell::new(0)))],
+            stack: vec![0],
             length,
         })
     }
@@ -26,38 +28,36 @@ impl Iterator for HandEnumerator {
     type Item = Vec<u8>;
 
     fn next(&mut self) -> Option<Self::Item> {
-        while let Some((start, i)) = self.stack.last().cloned() {
+        while let Some(i) = self.stack.pop() {
             if self.current_hand.len() == self.length {
                 let result = self.current_hand.clone();
                 // バックトラック処理
-                self.stack.pop();
                 if let Some(last_tile) = self.current_hand.pop() {
                     self.tiles[last_tile as usize] += 1;
                 }
                 return Some(result);
             }
 
-            if i.get() >= 34 {
+            if i >= NUM_TILE_INDEX {
                 // このレベルでのループ終了、バックトラック
-                self.stack.pop();
                 if let Some(last_tile) = self.current_hand.pop() {
                     self.tiles[last_tile as usize] += 1;
                 }
                 continue;
             }
 
-            if self.tiles[i.get()] > 0 {
+            if self.tiles[i] > 0 {
                 // タイルを選択
-                self.tiles[i.get()] -= 1;
-                self.current_hand.push(i.get() as u8);
+                self.tiles[i] -= 1;
+                self.current_hand.push(i as u8);
 
-                // 新しいフレームをスタックにプッシュ（再帰呼び出しのシミュレーション）
-                self.stack.push((i.get(), Rc::new(Cell::new(i.get()))));
                 // 現在のフレームのインデックスを更新
-                i.set(i.get() + 1);
+                self.stack.push(i + 1);
+                // 新しいフレームをスタックにプッシュ（再帰呼び出しのシミュレーション）
+                self.stack.push(i);
             } else {
                 // 次のタイルへ
-                i.set(i.get() + 1);
+                self.stack.push(i + 1);
             }
         }
 
