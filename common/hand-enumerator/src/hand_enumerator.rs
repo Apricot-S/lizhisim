a443e61f040c692948946517049732e54f680603
Apricot-S <mahjong_apricot@outlook.jp>
2024-09-16 01:44:29 +0900

    chore: Add unit tests

--- common/hand-enumerator/src/hand_enumerator.rs
+++ common/hand-enumerator/src/hand_enumerator.rs
@@ -64,3 +64,75 @@ impl Iterator for HandEnumerator {
         None
     }
 }
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use std::time::Instant;
+
+    #[test]
+    fn valid_length() {
+        (1..=MAX_NUM_HAND).into_iter().for_each(|i| {
+            let result = HandEnumerator::new(i);
+            assert!(result.is_ok())
+        });
+    }
+
+    #[test]
+    fn invalid_length() {
+        let result0 = HandEnumerator::new(0);
+        assert!(result0.is_err());
+        let result15 = HandEnumerator::new(MAX_NUM_HAND + 1);
+        assert!(result15.is_err());
+    }
+
+    fn correct_hands(
+        length: usize,
+        expected_first: &[u8],
+        expected_last: &[u8],
+        expected_count: usize,
+    ) {
+        let mut generator = HandEnumerator::new(length).unwrap();
+
+        let start = Instant::now();
+
+        let first_hand = generator.next().unwrap();
+        let (last_hand, count) = generator.fold((first_hand.clone(), 1), |(_, count), hand| {
+            (hand, count + 1)
+        });
+
+        let end = start.elapsed();
+        println!(
+            "Time elapsed in generating hands (length = {}) is: {:?}",
+            length, end,
+        );
+
+        assert_eq!(first_hand, expected_first);
+        assert_eq!(last_hand, expected_last);
+        assert_eq!(count, expected_count);
+    }
+
+    #[test]
+    #[ignore]
+    fn correct_hands_1() {
+        correct_hands(1, &[0], &[33], 34);
+    }
+
+    #[test]
+    #[ignore]
+    fn correct_hands_2() {
+        correct_hands(2, &[0, 0], &[33, 33], 595);
+    }
+
+    #[test]
+    #[ignore]
+    fn correct_hands_3() {
+        correct_hands(3, &[0, 0, 0], &[33, 33, 33], 7_140);
+    }
+
+    #[test]
+    #[ignore]
+    fn correct_hands_4() {
+        correct_hands(4, &[0, 0, 0, 0], &[33, 33, 33, 33], 66_045);
+    }
+}
