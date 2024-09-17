92abf941662c356c3615b1e7f4ed3a86830faf93
Apricot-S <mahjong_apricot@outlook.jp>
2024-09-17 18:07:25 +0000

    chore: Change the benchmark name

--- xiangting/benches/bench.rs
+++ xiangting/benches/bench.rs
@@ -6,21 +6,21 @@ const NUM_HAND: usize = 100_000_000;
 const SAMPLE_SIZE: usize = 10_000;
 const NUM_RESAMPLE: usize = 1_000;
 
-fn bench_xiangting(c: &mut Criterion) {
+fn xiangting_normal(c: &mut Criterion) {
     let mut rng = create_rng();
     let hands: Vec<_> = (0..NUM_HAND)
         .map(|_| generate_random_pure_hand(&mut rng))
         .collect();
 
-    let mut group = c.benchmark_group("bench_xiangting");
+    let mut group = c.benchmark_group("xiangting");
     group.sample_size(SAMPLE_SIZE);
     group.nresamples(NUM_RESAMPLE);
-    group.bench_function("bench_xiangting", |b| {
+    group.bench_function("normal", |b| {
         let mut hand = hands.iter();
         b.iter(|| calculate_replacement_number(hand.next().unwrap(), &None).unwrap())
     });
     group.finish();
 }
 
-criterion_group!(bench, bench_xiangting);
-criterion_main!(bench);
+criterion_group!(benches, xiangting_normal);
+criterion_main!(benches);
