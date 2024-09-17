f8b5ec9bb142101cb4235d5480666d43c29d4637
Apricot-S <mahjong_apricot@outlook.jp>
2024-09-18 04:02:15 +0900

    feat: Add Half Flush, Full Flush, and Thirteen Orphans modes.

--- common/random-hand/src/hand.rs
+++ common/random-hand/src/hand.rs
@@ -18,3 +18,73 @@ pub fn generate_random_pure_hand(rng: &mut impl Rng) -> [u8; 34] {
 
     hand
 }
+
+pub fn generate_random_half_flush_pure_hand(rng: &mut impl Rng) -> [u8; 34] {
+    let color_start = [0, 9, 18].choose(rng).unwrap();
+
+    let mut wall = [0u8; 64];
+    wall.iter_mut().enumerate().for_each(|(i, tile)| {
+        if i < 36 {
+            // Suits
+            *tile = (i / 4 + color_start) as u8;
+        } else {
+            // Honors
+            *tile = ((i - 36) / 4 + 27) as u8;
+        }
+    });
+    wall.shuffle(rng);
+
+    const CHOICES: [usize; 10] = [1, 2, 4, 5, 7, 8, 10, 11, 13, 14];
+    let hand_length = *CHOICES.choose(rng).unwrap();
+
+    let mut hand = [0u8; 34];
+    wall.iter()
+        .take(hand_length)
+        .for_each(|&t| hand[t as usize] += 1);
+
+    hand
+}
+
+pub fn generate_random_full_flush_pure_hand(rng: &mut impl Rng) -> [u8; 34] {
+    let color_start = [0, 9, 18].choose(rng).unwrap();
+
+    let mut wall = [0u8; 36];
+    wall.iter_mut()
+        .enumerate()
+        .for_each(|(i, tile)| *tile = (i / 4 + color_start) as u8);
+    wall.shuffle(rng);
+
+    const CHOICES: [usize; 10] = [1, 2, 4, 5, 7, 8, 10, 11, 13, 14];
+    let hand_length = *CHOICES.choose(rng).unwrap();
+
+    let mut hand = [0u8; 34];
+    wall.iter()
+        .take(hand_length)
+        .for_each(|&t| hand[t as usize] += 1);
+
+    hand
+}
+
+pub fn generate_random_thirteen_orphans_pure_hand(rng: &mut impl Rng) -> [u8; 34] {
+    const NON_SIMPLES: [u8; 13] = [0, 8, 9, 17, 18, 26, 27, 28, 29, 30, 31, 32, 33];
+    let mut wall = [0u8; 52];
+    NON_SIMPLES
+        .iter()
+        .cycle()
+        .take(52)
+        .enumerate()
+        .for_each(|(i, &tile)| {
+            wall[i] = tile;
+        });
+    wall.shuffle(rng);
+
+    const CHOICES: [usize; 10] = [1, 2, 4, 5, 7, 8, 10, 11, 13, 14];
+    let hand_length = *CHOICES.choose(rng).unwrap();
+
+    let mut hand = [0u8; 34];
+    wall.iter()
+        .take(hand_length)
+        .for_each(|&t| hand[t as usize] += 1);
+
+    hand
+}
--- common/random-hand/src/lib.rs
+++ common/random-hand/src/lib.rs
@@ -1,5 +1,8 @@
 mod hand;
 mod rng;
 
-pub use hand::generate_random_pure_hand;
+pub use hand::{
+    generate_random_full_flush_pure_hand, generate_random_half_flush_pure_hand,
+    generate_random_pure_hand, generate_random_thirteen_orphans_pure_hand,
+};
 pub use rng::create_rng;
