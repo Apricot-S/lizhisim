8c002dd9a2ed1efb630a63cc5abc7e14cd7a7589
Apricot-S <mahjong_apricot@outlook.jp>
2024-09-18 02:46:49 +0900

    docs: Update doctests

--- xiangting/src/calculate.rs
+++ xiangting/src/calculate.rs
@@ -29,26 +29,39 @@ pub enum XiangtingError {
 /// # Examples
 ///
 /// ```
-/// # use xiangting::calculate_replacement_number;
-/// // 123m456p789s1122z
-/// let hand_13: [u8; 34] = [
+/// # use xiangting::{calculate_replacement_number, ClaimedTilePosition, Mianzi};
+/// // 123m456p789s11222z
+/// let hand_14: [u8; 34] = [
 ///     1, 1, 1, 0, 0, 0, 0, 0, 0, // m
 ///     0, 0, 0, 1, 1, 1, 0, 0, 0, // p
 ///     0, 0, 0, 0, 0, 0, 1, 1, 1, // s
-///     2, 2, 0, 0, 0, 0, 0, // z
+///     2, 3, 0, 0, 0, 0, 0, // z
 /// ];
-/// let replacement_number_13 = calculate_replacement_number(&hand_13, &None);
-/// assert_eq!(replacement_number_13.unwrap(), 1u8);
 ///
-/// // 123m11z (3 melds)
+/// let replacement_number = calculate_replacement_number(&hand_14, &None);
+/// assert_eq!(replacement_number.unwrap(), 0u8);
+///
+/// // 123m1z (3 melds)
 /// let hand_4: [u8; 34] = [
 ///     1, 1, 1, 0, 0, 0, 0, 0, 0, // m
 ///     0, 0, 0, 0, 0, 0, 0, 0, 0, // p
 ///     0, 0, 0, 0, 0, 0, 0, 0, 0, // s
-///     2, 0, 0, 0, 0, 0, 0, // z
+///     1, 0, 0, 0, 0, 0, 0, // z
 /// ];
-/// let replacement_number_4 = calculate_replacement_number(&hand_4, &None);
-/// assert_eq!(replacement_number_4.unwrap(), 0u8);
+///
+/// // 456p 789s 111z
+/// let melds = Some([
+///     Some(Mianzi::Shunzi(12, ClaimedTilePosition::Low)),
+///     Some(Mianzi::Shunzi(24, ClaimedTilePosition::Low)),
+///     Some(Mianzi::Kezi(27)),
+///     None,
+/// ]);
+///
+/// let replacement_number_wo_melds = calculate_replacement_number(&hand_4, &None);
+/// assert_eq!(replacement_number_wo_melds.unwrap(), 1u8);
+///
+/// let replacement_number_w_melds = calculate_replacement_number(&hand_4, &melds);
+/// assert_eq!(replacement_number_w_melds.unwrap(), 2u8);
 /// ```
 pub fn calculate_replacement_number(
     bingpai: &Bingpai,
@@ -80,26 +93,39 @@ pub fn calculate_replacement_number(
 /// # Examples
 ///
 /// ```
-/// # use xiangting::calculate_xiangting_number;
-/// // 123m456p789s1122z
-/// let hand_13: [u8; 34] = [
+/// # use xiangting::{calculate_xiangting_number, ClaimedTilePosition, Mianzi};
+/// // 123m456p789s11222z
+/// let hand_14: [u8; 34] = [
 ///     1, 1, 1, 0, 0, 0, 0, 0, 0, // m
 ///     0, 0, 0, 1, 1, 1, 0, 0, 0, // p
 ///     0, 0, 0, 0, 0, 0, 1, 1, 1, // s
-///     2, 2, 0, 0, 0, 0, 0, // z
+///     2, 3, 0, 0, 0, 0, 0, // z
 /// ];
-/// let xiangting_number_13 = calculate_xiangting_number(&hand_13, &None);
-/// assert_eq!(xiangting_number_13.unwrap(), 0i8);
 ///
-/// // 123m11z (3 melds)
+/// let xiangting_number = calculate_xiangting_number(&hand_14, &None);
+/// assert_eq!(xiangting_number.unwrap(), -1i8);
+///
+/// // 123m1z (3 melds)
 /// let hand_4: [u8; 34] = [
 ///     1, 1, 1, 0, 0, 0, 0, 0, 0, // m
 ///     0, 0, 0, 0, 0, 0, 0, 0, 0, // p
 ///     0, 0, 0, 0, 0, 0, 0, 0, 0, // s
-///     2, 0, 0, 0, 0, 0, 0, // z
+///     1, 0, 0, 0, 0, 0, 0, // z
 /// ];
-/// let xiangting_number_4 = calculate_xiangting_number(&hand_4, &None);
-/// assert_eq!(xiangting_number_4.unwrap(), -1i8);
+///
+/// // 456p 789s 111z
+/// let melds = Some([
+///     Some(Mianzi::Shunzi(12, ClaimedTilePosition::Low)),
+///     Some(Mianzi::Shunzi(24, ClaimedTilePosition::Low)),
+///     Some(Mianzi::Kezi(27)),
+///     None,
+/// ]);
+///
+/// let xiangting_number_wo_melds = calculate_xiangting_number(&hand_4, &None);
+/// assert_eq!(xiangting_number_wo_melds.unwrap(), 0i8);
+///
+/// let xiangting_number_w_melds = calculate_xiangting_number(&hand_4, &melds);
+/// assert_eq!(xiangting_number_w_melds.unwrap(), 1i8);
 /// ```
 pub fn calculate_xiangting_number(
     bingpai: &Bingpai,
