007152722d68f0568297406833bca9ecaaa29ba5
Apricot-S <mahjong_apricot@outlook.jp>
2025-10-25 09:52:26 +0900

    feat: Develop tile

--- lizhisim/src/tile.rs
+++ lizhisim/src/tile.rs
@@ -2,6 +2,7 @@
 // SPDX-License-Identifier: MIT
 // This file is part of https://github.com/Apricot-S/lizhisim
 
+use crate::{matches_tu8, t, tu8};
 use thiserror::Error;
 
 const MAX_TILE_ID: u8 = 36;
@@ -32,16 +33,59 @@ impl Tile {
         Self { id }
     }
 
-    #[inline(always)]
+    #[inline]
     #[must_use]
-    pub const fn as_u8(&self) -> u8 {
+    pub(crate) const fn as_u8(&self) -> u8 {
         self.id
     }
 
-    #[inline(always)]
+    #[inline]
     #[must_use]
-    pub const fn to_usize(&self) -> usize {
+    pub(crate) const fn to_usize(&self) -> usize {
         self.id as usize
+    }
+
+    #[inline]
+    #[must_use]
+    pub(crate) const fn is_zipai(&self) -> bool {
+        matches_tu8!(self.id, 1z | 2z | 3z | 4z | 5z | 6z | 7z)
+    }
+
+    #[inline]
+    #[must_use]
+    pub(crate) const fn is_yaojiupai(&self) -> bool {
+        matches_tu8!(
+            self.id,
+            1m | 9m | 1p | 9p | 1s | 9s | 1z | 2z | 3z | 4z | 5z | 6z | 7z
+        )
+    }
+
+    #[inline]
+    #[must_use]
+    pub(crate) const fn is_hongbaopai(&self) -> bool {
+        matches_tu8!(self.id, 0m | 0p | 0s)
+    }
+
+    #[inline]
+    #[must_use]
+    pub(crate) const fn normalize_hongbaopai(&self) -> Self {
+        match self.id {
+            tu8!(0m) => t!(5m),
+            tu8!(0p) => t!(5p),
+            tu8!(0s) => t!(5s),
+            _ => *self,
+        }
+    }
+
+    #[inline]
+    #[must_use]
+    pub(crate) const fn decorate_hongbaopai(&self) -> Self {
+        match self.id {
+            tu8!(5m) => t!(0m),
+            tu8!(5p) => t!(0p),
+            tu8!(5s) => t!(0s),
+            _ => *self,
+        }
     }
 }
 
@@ -77,5 +121,79 @@ mod tests {
             Tile::try_from(37).unwrap_err(),
             TileError::OutOfRange(37)
         ));
+    }
+
+    #[test]
+    fn is_zipai_7z() {
+        let t = Tile::try_from(33).unwrap();
+        assert!(t.is_zipai());
+    }
+
+    #[test]
+    fn is_not_zipai_0m() {
+        let t = Tile::try_from(34).unwrap();
+        assert!(!t.is_zipai());
+    }
+
+    #[test]
+    fn is_yaojiupai_1m() {
+        let t = Tile::try_from(0).unwrap();
+        assert!(t.is_yaojiupai());
+    }
+
+    #[test]
+    fn is_not_yaojiupai_2m() {
+        let t = Tile::try_from(1).unwrap();
+        assert!(!t.is_yaojiupai());
+    }
+
+    #[test]
+    fn is_hongbaopai_0m() {
+        let t = Tile::try_from(34).unwrap();
+        assert!(t.is_hongbaopai());
+    }
+
+    #[test]
+    fn is_not_hongbaopai_7z() {
+        let t = Tile::try_from(33).unwrap();
+        assert!(!t.is_hongbaopai());
+    }
+
+    #[test]
+    fn normalize_hongbaopai_0m_to_5m() {
+        let t_0m = Tile::try_from(34).unwrap();
+        let t_5m = Tile::try_from(4).unwrap();
+        assert_eq!(t_0m.normalize_hongbaopai(), t_5m);
+    }
+
+    #[test]
+    fn normalize_hongbaopai_5m_to_5m() {
+        let t_5m = Tile::try_from(4).unwrap();
+        assert_eq!(t_5m.normalize_hongbaopai(), t_5m);
+    }
+
+    #[test]
+    fn normalize_hongbaopai_1m_to_1m() {
+        let t_1m = Tile::try_from(0).unwrap();
+        assert_eq!(t_1m.normalize_hongbaopai(), t_1m);
+    }
+
+    #[test]
+    fn decorate_hongbaopai_5p_to_0p() {
+        let t_5p = Tile::try_from(13).unwrap();
+        let t_0p = Tile::try_from(35).unwrap();
+        assert_eq!(t_5p.decorate_hongbaopai(), t_0p);
+    }
+
+    #[test]
+    fn decorate_hongbaopai_0p_to_0p() {
+        let t_0p = Tile::try_from(35).unwrap();
+        assert_eq!(t_0p.decorate_hongbaopai(), t_0p);
+    }
+
+    #[test]
+    fn decorate_hongbaopai_7z_to_7z() {
+        let t_7z = Tile::try_from(33).unwrap();
+        assert_eq!(t_7z.decorate_hongbaopai(), t_7z);
     }
 }
