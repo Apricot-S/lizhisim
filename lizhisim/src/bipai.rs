474636289c0000e73f9926b0c5aed19cddcd8e6e
Apricot-S <mahjong_apricot@outlook.jp>
2026-08-08 18:07:57 +0900

    remove old files

--- lizhisim/src/bipai.rs
+++
@@ -1,6 +0,0 @@
-// SPDX-FileCopyrightText: 2025 Apricot S.
-// SPDX-License-Identifier: MIT
-// This file is part of https://github.com/Apricot-S/lizhisim
-
-pub(crate) mod core;
-pub(crate) mod provider;
